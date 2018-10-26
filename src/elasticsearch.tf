903ad7fe23782307825ae1392ca713328f90226f
Daniel Budris <d.c.budris@gmail.com>
2018-10-25 20:03:26 -0400

    Update elasticsearch.tf

--- src/elasticsearch.tf
+++ src/elasticsearch.tf
@@ -1,8 +1,8 @@
 module "elasticsearch_demo" {
     source = "./main"
-    cluster-vpc-id = "vpc-150f1c70"
-    cluster-subnets = ["subnet-c23dd49a","subnet-c23dd49a"]
+    cluster-vpc-id = ""
+    cluster-subnets = ["",""]
     node-count = 1
     ec2_key_name = "admin_key"
-    elastic-password = "dGVzdDEyMzQ1Ng=="
+    elastic-password = ""
 }
